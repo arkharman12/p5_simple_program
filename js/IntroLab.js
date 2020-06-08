33f3d3c0882d379fb3ab8a8931be71e8390c03f0
Harman Ark <ark.harman@gmail.com>
2020-06-08 11:58:12 -0400

    Final commit

---
+++ js/IntroLab.js
@@ -0,0 +1,17 @@
+/*
+
+    N220 Section 25750
+    Harmanjot Singh
+    Intro Lab
+    15 January 2020
+
+*/
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background("darkblue");
+    circle(400, 300, 100);
+}
